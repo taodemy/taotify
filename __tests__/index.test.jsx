8acfefacef2e63f6ce88e2d81ff3386cabdfd26a
Zihan YU <deepmemorysea@163.com>
2023-02-28 19:50:15 +1100

    feat: init

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../src/pages/index";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const main = screen.getByRole("main");
+    expect(main).toBeInTheDocument();
+  });
+});
