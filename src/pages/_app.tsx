80b153aa68431fd04b248ef1868d1949fe4ec2e4
huchshm <84855079+huchshm@users.noreply.github.com>
2023-03-15 11:59:16 +1030

    feat: setup useContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import MyContext from "contexts/MyContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MyContext.Provider value={{ name: "John", age: 30, occupation: "Developer" }}>
+      <Component {...pageProps} />
+    </MyContext.Provider>
+  );
 }
