275dbf91bd4c9e3248f7cd2b258e2fc3c040c03b
LULIU-9 <lu.liu4work@gmail.com>
2023-03-23 16:49:34 +1000

    add font-family in _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
 import { MusicContextProvider } from "contexts/MusicContext";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
