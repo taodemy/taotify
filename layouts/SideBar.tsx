36371f66c9cdbaec54a0f851d962567826810f94
Charles <xingbin11223344@gmail.com>
2023-03-26 14:02:11 +1030

    use instead of div

--- layouts/SideBar.tsx
+++ layouts/SideBar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function SideBar() {
   return (
-    <div className=" w-[20vh] h-[100vh] fixed top-0 left-0 border-2 border-dashed border-black">
+    <aside className=" w-[10vw] h-[100vh] fixed top-0 left-0 border-2 border-dashed border-black">
       This is the left SideBar
-    </div>
+    </aside>
   )
 }
