743424af69315577bc00e316857934fb6ee87ca6
Charles <xingbin11223344@gmail.com>
2023-03-27 20:21:10 +1030

    hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./layouts/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     colors: {
       primary: {
