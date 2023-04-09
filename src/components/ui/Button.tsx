4170906e64a4d11ae4aa983fd4abed3a5847c1ea
Trent1900 <lc20200212@gmail.com>
2023-04-10 08:43:21 +1000

    refine button props

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,14 @@
 import CloseIcon from "./CloseIcon";
 
 type ButtonColor = "primary" | "secondary" | "ternary" | "warning" | "info" | "light" | "dark";
-export type Props = {
+type ButtonProps = {
   color?: ButtonColor;
   size?: "tiny" | "small" | "normal" | "large";
   outline?: boolean;
   variant?: "normal" | "icon";
   label?: string;
   onClick?: () => void;
-};
+} & React.ComponentProps<"button">;
 
 const btn = " text-light text-base rounded-full font-[Roboto] flex justify-center items-center";
 const getButtonClass = (color: ButtonColor, outline: boolean) => {
@@ -62,7 +62,7 @@ const Button = ({
   onClick,
   label = "button",
   ...otherProps
-}: React.ButtonHTMLAttributes<HTMLButtonElement> & Props) => {
+}: ButtonProps) => {
   const buttonColor = {
     primary: getButtonClass("primary", outline),
     secondary: getButtonClass("secondary", outline),
