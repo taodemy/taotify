b1946e02963db219d3e87dcc9ea9e8e47935a409
Young <yqin7516@gmail.com>
2023-04-12 18:56:36 +1000

    update: slider function logic

--- src/components/VolumeSlideController.tsx
+++ src/components/VolumeSlideController.tsx
@@ -7,25 +7,31 @@ export default function VolumeSlideController() {
   const volumeSlider = useRef<HTMLDivElement>(null);
   const handleDragSlideStart = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
+    const newVolumeLevel = updateVolumeLevel(event);
+    setVolumeLevel(newVolumeLevel);
     document.addEventListener("mousemove", handleDragSlideMove);
     document.addEventListener("mouseup", handleDragSlideEnd);
   };
-  const handleDragSlideMove = (event: MouseEvent | React.MouseEvent<HTMLDivElement>) => {
-    const rect = volumeSlider.current!.getBoundingClientRect();
-    const width = event.clientX - rect.left;
-    const newVolumeLevel = volumeUtils.calculateVolume(width, rect.width);
+  const handleDragSlideMove = (event: MouseEvent) => {
+    const newVolumeLevel = updateVolumeLevel(event);
     setVolumeLevel(newVolumeLevel);
   };
   const handleDragSlideEnd = () => {
     document.removeEventListener("mousemove", handleDragSlideMove);
     document.removeEventListener("mouseup", handleDragSlideEnd);
   };
+  const updateVolumeLevel = (event: MouseEvent | React.MouseEvent<HTMLDivElement>) => {
+    const rect = volumeSlider.current!.getBoundingClientRect();
+    const width = event.clientX - rect.left;
+    const newVolumeLevel = volumeUtils.calculateVolume(width, rect.width);
+    return newVolumeLevel;
+  };
   return (
     <div className={`w-52 absolute right-10 bottom-5 mx-auto`} data-testid="volumeSlideContainer">
       <div
         ref={volumeSlider}
         className={`relative h-5 rounded cursor-pointer bg-primary-100`}
-        onClick={handleDragSlideMove}
+        onMouseDown={handleDragSlideStart}
         data-testid="volumeSlideBar"
       >
         <div
@@ -36,7 +42,6 @@ export default function VolumeSlideController() {
         <div
           className={`absolute w-5 h-5 rounded-full bg-primary-200 top-1/2 transform -translate-y-1/2 cursor-pointer`}
           style={{ left: `calc(${volumeLevel}% - ${halfThumb}rem)` }}
-          onMouseDown={handleDragSlideStart}
           data-testid="volumeSlideThumb"
         ></div>
       </div>
