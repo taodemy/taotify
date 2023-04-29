9afe828fadb43f3ac809b476c12bf71e43c66923
Trent <63524682+Trent1900@users.noreply.github.com>
2023-04-29 20:54:50 +1000

    fix: add placeholder to component

--- src/components/Input/Search.tsx
+++ src/components/Input/Search.tsx
@@ -4,9 +4,14 @@ import { AiOutlineSearch } from "react-icons/ai";
 interface ISearchProps {
   onSubmit?: React.FormEventHandler<HTMLFormElement> | (() => void);
   onClick?: React.MouseEventHandler<SVGElement> | (() => void);
+  placeholder?: string;
 }
 
-const Search = ({ onSubmit, onClick }: ISearchProps) => {
+const Search = ({
+  onSubmit,
+  onClick,
+  placeholder = "Search for music, album and artist",
+}: ISearchProps) => {
   const [inputValue, setInputValue] = useState<string>("");
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,7 +28,7 @@ const Search = ({ onSubmit, onClick }: ISearchProps) => {
       </div>
       <input
         id="searchInput"
-        placeholder="Search for music, album and artist"
+        placeholder={placeholder}
         className="hidden h-9 w-full rounded-full border border-light-100 bg-black/75 py-2 px-3 pl-9 text-xl text-light-400 placeholder:text-light-100
           hover:border-light-400 hover:placeholder:text-light-400 focus:border-light-400 focus:outline-none focus:placeholder:text-transparent sm:flex"
         onChange={handleInput}
