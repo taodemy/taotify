66eeb842f1b11edf69c45a4ce538dfebf592cf6f
Prom1s1ngYoung <yqin7516@gmail.com>
2023-06-13 15:15:32 +1000

    update: volume control implementing

--- src/components/volumeController/index.tsx
+++ src/components/volumeController/index.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import VolumeMuteController from "./VolumeMuteController";
 import VolumeSlideController from "./VolumeSlideController";
+import { VolumeContext } from "@/contexts/VolumeContext";
+import { WebAudioContext } from "@/contexts/WebAudioContext";
 
 export default function VolumeController() {
+  const { volumeLevel } = useContext(VolumeContext);
+  const { gainNode } = useContext(WebAudioContext);
+  useEffect(() => {
+    if (gainNode) {
+      gainNode.gain.value = volumeLevel * 0.01;
+    }
+  }, [volumeLevel]);
   return (
     <div className="right-50 absolute top-20 flex gap-2">
       <VolumeMuteController />
