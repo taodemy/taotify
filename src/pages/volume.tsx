10090f2a999c7764868d21b1e3525ce7aa61b230
Prom1s1ngYoung <yqin7516@gmail.com>
2023-06-29 14:21:33 +1000

    update: audio visualizer

--- src/pages/volume.tsx
+++ src/pages/volume.tsx
@@ -1,8 +1,16 @@
+import AudioVisualizer from "@/components/visualizer";
 import VolumeController from "@/components/volumeController";
 import { VolumeContextProvider } from "@/contexts/VolumeContext";
+import { useEffect, useState } from "react";
 const VolumeControllerDemo = () => {
+  const [isClientSide, setIsClientSide] = useState<boolean>(false);
+  useEffect(() => {
+    if (typeof window !== "undefined") setIsClientSide(true);
+  }, []);
+
   return (
     <VolumeContextProvider>
+      {isClientSide ? <AudioVisualizer /> : null}
       <VolumeController></VolumeController>
     </VolumeContextProvider>
   );
