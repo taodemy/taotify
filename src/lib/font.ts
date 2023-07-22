57a71c9ade61632a8f9bfdbedf229a44fb9f4f4f
Trent1900 <lc20200212@gmail.com>
2023-07-22 22:45:57 +1000

    test storybook

---
+++ src/lib/font.ts
@@ -0,0 +1,19 @@
+import { Roboto, Allura,Sora } from "next/font/google";
+
+export const roboto = Roboto({
+  weight: ["300", "400", "500"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
+export const allura = Allura({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-allura",
+});
+
+
+export const sora = Sora({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-sora",
+});
