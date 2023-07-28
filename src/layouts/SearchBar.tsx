1ffaf5c9b2569cd3280c5ee0476cb7540202761e
Trent1900 <lc20200212@gmail.com>
2023-07-28 17:39:34 +1000

    remove unwanted import

--- src/layouts/SearchBar.tsx
+++ src/layouts/SearchBar.tsx
@@ -1,15 +1,9 @@
 import IconButton from "@/components/buttons/IconButton";
-import LinkButton from "@/components/buttons/LinkButton";
 import Search from "@/components/Input/Search";
 import Link from "next/link";
 import React from "react";
-import { AiOutlineSetting } from "react-icons/ai";
 
 const SearchBar = () => {
-  const handleSetting = () => {};
-  const handleNotification = () => {};
-  const handleSearch = () => {};
-
   return (
     <div className="flex w-full items-center bg-dark-200 md:left-[64px] lg:left-[320px]">
       <Link className="mr-2 text-light" href={"/"}>
