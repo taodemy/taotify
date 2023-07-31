17ec03fae9bc8420c7b4328464370669217687c2
Trent1900 <lc20200212@gmail.com>
2023-07-31 14:08:47 +1000

    adjust search padding

--- src/layouts/SearchBar.tsx
+++ src/layouts/SearchBar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const SearchBar = () => {
   return (
-    <div className="flex w-full items-center bg-dark-200 md:left-[64px] lg:left-[320px]">
+    <div className="flex w-full items-center bg-dark p-2 pb-8 md:px-6 md:pt-6 lg:px-10 lg:pt-8">
       <Link className="mr-2 text-light" href={"/"}>
         Home
       </Link>
