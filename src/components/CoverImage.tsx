3a2091df40fb9afaa61a9386da183bafa5b079cc
Prom1s1ngYoung <yqin7516@gmail.com>
2023-09-18 20:27:20 +1000

    update: handle click event
    move handle click event from img tag to a tag

--- src/components/CoverImage.tsx
+++ src/components/CoverImage.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useRouter } from "next/router";
-import Image from "next/image";
 
 interface CoverImageProps {
   variant?: "avatars" | "musicCover";
@@ -17,14 +16,15 @@ const CoverImage = ({ variant = "musicCover", src }: CoverImageProps) => {
     <img className="rounded-full" src={src} alt={`Picture of ${variant}`} width={48} height={48} />
   ) : (
     <div className="h-[64px] w-[64px] overflow-hidden md:h-[60px] md:w-[60px] lg:h-[100px] lg:w-[100px]">
-      <img
-        className="rounded-full"
-        src={src}
-        alt={`Picture of ${variant}`}
-        width={100}
-        height={100}
-        onClick={handleClickToVisualizer}
-      />
+      <a onClick={handleClickToVisualizer}>
+        <img
+          className="rounded-full"
+          src={src}
+          alt={`Picture of ${variant}`}
+          width={100}
+          height={100}
+        />
+      </a>
     </div>
   );
 };
