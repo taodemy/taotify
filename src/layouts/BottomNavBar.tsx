ba08dfa1d8cd8f8f0f388400042e68dce9e7afdd
Jason Wang <0212jason@gmail.com>
2023-10-18 15:48:52 +1000

    feat: implement the resposive sidebar

--- src/layouts/BottomNavBar.tsx
+++ src/layouts/BottomNavBar.tsx
@@ -1,14 +1,33 @@
 import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import Image from "next/image";
+import { menuList } from "@/components/menu";
 
 const BottomNavBar = () => {
+  const currentPath = window.location.pathname;
+
   return (
     <section className="fixed bottom-[0px] h-[72px] w-full border-t transition-all duration-200 ease-in-out md:h-0">
       <div className="relative h-full drop-shadow-bgImgShadow">
         <Image src="/bg_player.png" alt="Player background image" fill />
       </div>
-      <div className="absolute left-0 top-0 flex h-full w-full gap-2 bg-dark-400 bg-opacity-80 px-4 py-2 backdrop-blur-2xl">
-        <p className="flex items-center text-light">This is the bottom navigation bar.</p>
+      <div className="absolute top-0 left-0 flex h-full w-full gap-2 bg-dark-400 bg-opacity-80 backdrop-blur-2xl md:hidden">
+        {
+          <ul className="mx-auto flex text-light-200">
+            {Object.keys(menuList).map((menuItem, index) => (
+              <Link className="flex" key={index} href={menuList[menuItem].path}>
+                <li
+                  className={`flex items-center px-6 ${
+                    currentPath === menuList[menuItem].path && "bg-primary"
+                  }`}
+                >
+                  {menuList[menuItem].icon}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        }
       </div>
     </section>
   );
--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,10 +1,95 @@
 import AudioVisualizer from "@/components/visualizer";
-import React from "react";
+import React, { useContext } from "react";
+import Link from "next/link";
+import { MusicContext } from "@/contexts/MusicContext";
+import { SiYoutubemusic } from "react-icons/si";
+import { IoEllipsisHorizontalSharp } from "react-icons/io5";
+import { MdQueueMusic } from "react-icons/md";
+import { RiHeartLine, RiDeleteBinLine } from "react-icons/ri";
+import { FiPlus } from "react-icons/fi";
+import { TbClockPlay } from "react-icons/tb";
+import { menuList } from "@/components/menu";
 
 const SideBar = () => {
+  const { selectedAlbum } = useContext(MusicContext);
+  const musicContext = selectedAlbum?.musicContext;
+  const currentPath = window.location.pathname;
+
   return (
-    <aside className="fixed bottom-0 top-0 w-0 flex-col bg-black text-white md:flex md:w-[64px] lg:w-[320px]">
-      This is the left SideBar
+    <aside>
+      {musicContext && (
+        <div className="relative h-full drop-shadow-bgImgShadow">
+          <img src={musicContext[0]?.artist?.image} alt="Sidebar artist image" />
+        </div>
+      )}
+      <div className="fixed bottom-0 top-0 w-0 flex-col bg-dark-400 bg-opacity-80 text-white backdrop-blur-2xl md:flex md:w-[64px] lg:w-[320px]">
+        <div className="mt-8 flex items-center justify-center gap-2 font-roboto lg:ml-[36px] lg:justify-start">
+          <SiYoutubemusic className="h-10 w-10" />
+          <h2 className="hidden lg:block">Taotify</h2>
+          <IoEllipsisHorizontalSharp className="ml-[63px] hidden h-8 w-8 lg:block" />
+        </div>
+        <div className="flex flex-col font-roboto text-lg lg:items-start">
+          <div className="invisible mb-8 mt-[54px] text-primary-100 lg:visible lg:ml-[36px]">
+            MENU
+          </div>
+          {
+            <ul className="flex w-full flex-col gap-8 font-roboto text-lg text-light-200">
+              {Object.keys(menuList).map((menuItem, index) => (
+                <Link className="flex w-full" key={index} href={menuList[menuItem].path}>
+                  <div
+                    className={`h-8 w-2 ${currentPath === menuList[menuItem].path && "bg-primary"}`}
+                  ></div>
+                  <li className={`flex w-full items-center`}>
+                    <div
+                      className={`${
+                        currentPath === menuList[menuItem].path &&
+                        "bg-gradient-to-r from-primary to-transparent"
+                      } flex h-full items-center pl-3 pr-5 lg:pl-6`}
+                    >
+                      {menuList[menuItem].icon}
+                    </div>
+                    <p className="hidden lg:block">{menuItem}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          }
+          <div className="invisible mt-12 mb-8 text-primary-100 lg:visible lg:ml-[36px] ">
+            YOUR LIBRARY
+          </div>
+          <div className="flex flex-col items-center gap-8 font-roboto text-lg text-light-200 lg:ml-[36px]">
+            <div className="flex items-center gap-4">
+              <TbClockPlay className="h-6 w-6" />
+              <p className="hidden lg:block">Recently Played</p>
+            </div>
+            <div className="flex items-center gap-4">
+              <RiHeartLine className="h-6 w-6" />
+              <p className="hidden lg:block">Favourite songs</p>
+            </div>
+          </div>
+          <div className="mt-12 mb-8 flex items-center gap-[146px] lg:ml-[36px] ">
+            <p className="invisible text-primary-100 lg:visible">PlAYLISTS</p>
+            <FiPlus className="h-6 w-6" />
+          </div>
+          <div className="flex flex-col items-center gap-8 font-roboto text-lg text-light-200 lg:ml-[36px]">
+            <div className="flex items-center">
+              <MdQueueMusic className="h-6 w-6" />
+              <p className="ml-4 mr-[51px] hidden lg:block">Top Hit 2023 - USA</p>
+              <RiDeleteBinLine className="hidden h-6 w-6 lg:block" />
+            </div>
+            <div className="flex items-center">
+              <MdQueueMusic className="h-6 w-6" />
+              <p className="ml-4 mr-[51px] hidden lg:block">Top Hit 2023 - USA</p>
+              <RiDeleteBinLine className="hidden h-6 w-6 lg:block" />
+            </div>
+            <div className="flex items-center">
+              <MdQueueMusic className="h-6 w-6" />
+              <p className="ml-4 mr-[51px] hidden lg:block">Top Hit 2023 - USA</p>
+              <RiDeleteBinLine className="hidden h-6 w-6 lg:block" />
+            </div>
+          </div>
+        </div>
+      </div>
     </aside>
   );
 };
